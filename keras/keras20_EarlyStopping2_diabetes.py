1f0d8a5bcb3c6c918d009b47b9ed5ea9c922e3c3
Im Kyeongbin <fjeidjenk38@gmail.com>
2026-09-08 17:59:23 +0900

    텐서플로 7일차, binary_crossentropy y_pred = np.round(y_pred)

--- keras/keras20_EarlyStopping2_diabetes.py
+++ keras/keras20_EarlyStopping2_diabetes.py
@@ -20,6 +20,7 @@ model.add(Dense(16))
 model.add(Dense(10))
 model.add(Dense(1))
 
+
 #3. 컴파일, 훈련
 model.compile(loss = 'mse', optimizer = 'adam')
 
@@ -28,7 +29,7 @@ from keras.callbacks import EarlyStopping
 es = EarlyStopping(
     monitor='val_loss',
     mode='auto',
-    patience=10,
+    patience=20,
     restore_best_weights=True,
 )
 
@@ -44,7 +45,10 @@ print('r2 :', r2)
 # r2(20-10-1) : 0.5189658962934192
 # r2(20-15-10-1) : 0.394818920018537
 # r2(20-16-10-1) : 0.526455363169092
-
+# r2(24-20-8-1) : 0.4379119991942728
+# r2(24-12-8-1) : 0.43607822960854803
+# r2(20-12-8-1) : 0.43489047037394446
+# r2(20-16-10-1) : 0.42782492506749203
 print("========================= hist =========================")
 print(hist)
 print("========================= hist.history =======================")
